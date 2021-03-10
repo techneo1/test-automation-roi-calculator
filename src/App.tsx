e21ba04fce5808600cc9b6d73737f872387bc90a
Srikanth Shet <techneo1@gmail.com>
2021-03-10 12:24:59 +0530

    Add chart and support for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import "./App.scss";
+import { LineChart } from 'react-chartkick'
+import 'chart.js'
+import './App.scss';
 
 export default function App() {
+  const savingsChartData = [
+    { "name": "Total Savings due to Automation", "data": { "now": 0, "1 years": 160000, "2 years": 240000, "3 years": 320000 } }
+  ];
+
+  const roiChartData = [
+    { "name": "Effective ROI", "data": { "now": 0, "1 years": 43, "2 years": 114, "3 years": 186 } }
+  ];
+
   return (
     <div className="container">
-      <h2 className="text-center m-4">Test Automation ROI Calculator</h2>
-      {/* <p className="text-center">some description goes here...</p> */}
+      <h1 className="text-center m-4">Test Automation ROI Calculator</h1>
       <div className="row mb-5">
         <div className="col-md-2"></div>
         <div className="col-12 col-md-9 col-xl-8 center">
-          {/* <p className="fancy-title">
-            Enter below data and know the projection of
-              <strong> Savings from Automation</strong> and
-                  <strong> ROI </strong>
-              upto the next 5 years in a blink!
-          </p> */}
-          <div className="alert alert-light">
-            Know the projection of
-              <strong> Savings from Automation</strong> and
-                  <strong> ROI </strong>
-              upto the next 5 years in a blink!
-                </div>
-
-          <div className="card">
-            {/* <h4 className="card-header">1. Calculate ROI</h4> */}
+          <div className="card form">
             <div className="card-body">
               <form>
                 <div className="form-group">
@@ -124,6 +119,33 @@ export default function App() {
             </div>
           </div>
         </div>
+
+        <div className="card-deck mt-5 mx-auto">
+          <div className="card">
+            <div className="card-body text-center">
+              <h1 className="card-title">€ 320,000</h1>
+              <h3 className="card-title">Savings<sup>*</sup></h3>
+              <LineChart
+                data={savingsChartData}
+                prefix="€"
+                thousands=","
+                dataset={{ borderColor: "#ff5000", pointBackgroundColor: "#777777", pointBorderColor: "#777777", pointStyle: "rectRounded" }}
+              />
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body text-center">
+              <h1 className="card-title">186</h1>
+              <h3 className="card-title">ROI<sup>*</sup></h3>
+              <LineChart
+                data={roiChartData}
+                dataset={{ borderColor: "#498fab", pointBackgroundColor: "#777777", pointBorderColor: "#777777", pointStyle: "rectRounded" }}
+              />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div >
   );
